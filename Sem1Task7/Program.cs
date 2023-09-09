d90e328d4e4e2fb94f7a4b45e0739b8e283f8abe
Ilya.Khanin <orange.flow@yandex.ru>
2023-09-09 20:22:18 +0400

    solution from sem 1 last task

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №7
+
+int num = int.Parse(Console.ReadLine()??"0");
+if(num>100 && num<1000)
+{
+    int lastDigit = num%10;
+    Console.WriteLine(" Последняя цифра числа " +num+ " это " +lastDigit);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
